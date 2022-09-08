c429e793563e0a36536db1f8bee534f8d45b7756
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-08 16:41:30 -0700

    Put.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+nclude <stdio.h>                                                                                                                      
+#include <string.h>                                                                                                                     
+                                                                                                                                        
+int main (void)                                                                                                                         
+{                                                                                                                                    
+char str[]= "\"Programming is like bulding a multilingual puzzle";                                                           
+		                                                                                                                        puts(str);                                                                                                                   
+			                                                                                                                return(0);                                                                                                                   
+}        
