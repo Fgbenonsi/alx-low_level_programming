f5357bee8bed8ef4771b3485404004e1a6748632
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-08 17:06:14 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
-#include <stdio.h>                                                                                                                      
-#include <string.h>                                                                                                                     
-                                                                                                                                        
-int main (void)                                                                                                                         
-{                                                                                                                                    
-char str[]= "\"Programming is like building a multilingual puzzle";
-puts(str);
-return(0);                                                                                                                   
-}        
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -Main function for entry
+ *
+ *
+ * return -Return 0 there is no called function
+ */
+int main (void)
+{
+	char str[]= "\"Programming is like building a multilingual puzzle";
+
+	put(str);
+	return (0);
+}
