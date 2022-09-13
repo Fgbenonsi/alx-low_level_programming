b1e9e5b68bfb28b37e807cfc5a8cd0652480e77c
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-13 15:49:35 -0700

    ptchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	x = 0;
+
+	while(x = 0)
+	{
+		printf("_putchar\n");
+		++x;
+	}
+	return (0);
+}
