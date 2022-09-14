60041cd3fb6bfddbf3f6b176a71b7bbc3a13d167
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-13 17:29:16 -0700

    Alh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,15 @@
 /**
  * main - Entry point
  *
- * print_alphabet - print in lower case a to z
+ * print_alphabet - use in _putchar funtion to print in lower case a to z
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	int b;
 
-	for (b = 'a'; b <= 'z'; ++b)
+	for (b = 'a', b <= 'z'; ++b)
 		_putchar(b);
 	_putchar('\n');
 }
