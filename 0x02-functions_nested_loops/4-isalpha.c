abc023bb6bfabf54756ad0b56de25d22ad0ba499
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-13 18:11:34 -0700

    alp

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include"main"
+
+/**
+ * _isaplha - ajust character in lowercase and uppercase
+ *
+ * @c: input from other function
+ *
+ * Return: return 1 if it is c otherwise always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
