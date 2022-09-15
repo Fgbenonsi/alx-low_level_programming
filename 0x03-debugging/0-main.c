18afcd4e8ac6e5932f76371b0ffb9a83638a4581
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-15 14:06:59 -0700

    Main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main - print if int is postive of nagative
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	int = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
