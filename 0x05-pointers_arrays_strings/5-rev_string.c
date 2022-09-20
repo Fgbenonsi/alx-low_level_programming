4f5004857ab73d40228759f617377470ecbc19ba
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-19 18:17:32 -0700

    REVSTR

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,30 @@
-#include 
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: the sting
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	char *r = s;
+	char x[1000];
+	short v = 0;
+
+	while (*s != '\0')
+	{
+		x[v] = *s;
+		s++;
+		v++;
+	}
+	v = 0;
+
+	while (s > r)
+	{
+		s--;
+		*s = x[v];
+		v++;
+	}
+}
+
