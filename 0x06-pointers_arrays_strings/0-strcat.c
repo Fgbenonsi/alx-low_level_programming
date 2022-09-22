bec2b00daee5d5a17c629340699349108c94171b
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-22 14:28:22 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates strings
+ * @dest: string to be appended
+ * @scr: string to be concatenated
+ *
+ * Return: return to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_length = 0;
+
+	while (dest[index++])
+		dest_length++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_length++] = src[index];
+
+	return (dest);
+}
