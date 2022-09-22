1de2ac869251418fd9a46127df4aedd8602f795d
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-22 15:30:27 -0700

    STRNCAT

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates strings but add n number of bytes
- * @dest: string to be appended
- * @src: string to be concatenated
- * n: integer to compare with index
+ * _strncat - concatenates string but add n numbers
+ * @dest: the string to be appended
+ * @src: the string to be concatenated
+ * n: factor to compare with ind
  * Return: returns dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, dest_l = 0;
 
 	while (dest[index++])
-		dest_len++;
+		dest_l++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+		dest[dest_l++] = src[index];
 
 	return (dest);
 }
