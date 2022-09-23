97b14b45a526f9a5401bdf44dd614fe1c6e0f6b3
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-22 17:40:57 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * leet - leet encorder
+ * @str: this is the string to be encoded
+ * Return: address of the string
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform(str[i]);
+		i++;
+	}
+	return (str);
+}
