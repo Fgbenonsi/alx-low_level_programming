b270bd9d54d91dc8a20620398d53a2d1f448b1a9
Fgbenonsi <gbenonsifab@yahoo.com>
2022-09-22 18:05:52 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,3 +18,28 @@ char *leet(char *str)
 	}
 	return (str);
 }
+
+/**
+ * transform - function to map a letter
+ * @x: char to be encoded
+ * Return: char
+ */
+
+char transform(char x)
+{
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 8)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break;
+		}
+		i++;
+	}
+	return (replacement);
+}
